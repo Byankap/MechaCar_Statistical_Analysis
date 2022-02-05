097f4c1d7923cb54776bc197e02b0088e3b52c15
Byanka Partlow <byanka.partlow@gmail.com>
2022-02-04 23:20:55 -0800

    Challenge_15

---
+++ MechaCarChalleng.R
@@ -0,0 +1,8 @@
+# Use the library() function to load the dplyr package
+library(dplyr)
+#Import and read in the MechaCar_mpg.csv file as a dataframe.
+library(tidyverse)
+mpg_data <- read.csv(file='Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+#Perform linear regression using the lm() function
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mpg_data) #generate multiple linear regression model
+#using the summary() function, determine the p-value and the r-squared value for the linear regression model.
